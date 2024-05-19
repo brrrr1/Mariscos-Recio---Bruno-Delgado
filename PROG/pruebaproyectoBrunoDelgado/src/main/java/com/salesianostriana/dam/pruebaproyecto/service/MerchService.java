091fcfbffce5180dc95a8fef19e339e55f323b8e
br1 <delgado.hebru24@triana.salesianos.edu>
2024-05-19 13:13:19 +0200

    Filtros implementados

--- PROG/pruebaproyectoBrunoDelgado/src/main/java/com/salesianostriana/dam/pruebaproyecto/service/MerchService.java
+++ PROG/pruebaproyectoBrunoDelgado/src/main/java/com/salesianostriana/dam/pruebaproyecto/service/MerchService.java
@@ -4,6 +4,9 @@ package com.salesianostriana.dam.pruebaproyecto.service;
 
 
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 
 import com.salesianostriana.dam.pruebaproyecto.base.BaseServiceImpl;
@@ -15,7 +18,40 @@ public class MerchService extends BaseServiceImpl<Merch, Long, MerchRepositorio>
 	
 	
 
-
+	public List<Merch> ordenarAlfabeticoAZ(){
+	return this.repository.ordenarAlfabeticoAZ();
+	}
+	
+	
+	public List<Merch> ordenarAlfabeticoZA(){
+		return this.repository.ordenarAlfabeticoZA();
+	}
+	
+	
+	public List<Merch> ordenarLikesMayor(){
+		return this.repository.ordenarLikesMayor();
+	}
+	
+	
+	public List<Merch> ordenarLikesMenor(){
+	return this.repository.ordenarLikesMenor();
+	}
+	
+	public List<Merch> ordenarDescMayor(){
+		return this.repository.ordernarDescuentoMayor();
+		}
+	
+	public List<Merch> ordenarDescMenor(){
+		return this.repository.ordernarDescuentoMenor();
+		}
+	
+	public List<Merch> ordenarPrecioMayor(){
+		return this.repository.ordernarPrecioMayor();
+		}
+	
+	public List<Merch> ordenarPrecioMenor(){
+		return this.repository.ordernarPrecioMenor();
+		}
 	
 	
 	
